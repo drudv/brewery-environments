8b38a2b4674151e9fbd222c9b7825d6fbfdd864d
Dmitry Druganov <dmitry.druganov@citrix.com>
2020-06-23 17:54:03 +0200

    Add API authorization

--- index.js
+++ index.js
@@ -2,11 +2,42 @@ const express = require('express');
 const http = require('http');
 const Pool = require('pg').Pool;
 
-console.log('process.env.DATABASE_URL', process.env.DATABASE_URL);
+const authMiddleware = async (req, res, next) => {
+  try {
+    const token = (req.headers.authorization || '').split(' ')[1];
+    if (!token) {
+      throw 'Empty authorization token';
+    }
+    await validateAPIToken(token);
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401);
+  }
+};
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
+
+const validateAPIToken = (token) => {
+  return new Promise((resolve, reject) => {
+    pool.query(
+      'SELECT id FROM api_tokens WHERE token = $1',
+      [token],
+      (error, results) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        if (results.rows.length === 0) {
+          reject(new Error('No such authorization token'));
+        }
+        resolve();
+      }
+    );
+  });
+};
 
 const getEnvironments = () => {
   return new Promise((resolve, reject) => {
@@ -27,7 +58,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/environment', async (req, res) => {
+app.get('/api/v0/environment', authMiddleware, async (req, res) => {
   const envs = await getEnvironments();
   return res.send(envs);
 });
